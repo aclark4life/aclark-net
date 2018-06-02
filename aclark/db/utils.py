9fed85df224e6e3afecfd679a9d4aa0fa09f0d68
Alex Clark <aclark@aclark.net>
2018-06-02 11:20:06 -0400

    Update

--- aclark/db/utils.py
+++ aclark/db/utils.py
@@ -15,7 +15,6 @@ from .info import get_note_info
 from .info import get_recipients
 from .info import get_setting
 from .obj import get_template_and_url
-from .obj import get_times_for_obj
 from .obj import obj_copy
 from .obj import obj_redir
 from .obj import obj_remove
@@ -397,7 +396,7 @@ def get_page_items(**kwargs):
             context['item'] = file_obj
         elif model_name == 'invoice':
             invoice = get_object_or_404(model, pk=pk)
-            times = get_times_for_obj(invoice, time_model)
+            times = time_model.objects.filter(estimate=None, invoice=invoice)
             times = times.order_by(*order_by['time'])
             times = set_total_amount(times, invoice=invoice)
             last_payment_date = invoice.last_payment_date
@@ -424,7 +423,8 @@ def get_page_items(**kwargs):
             estimates = estimate_model.objects.filter(
                 project=project, is_to=False, is_sow=False)
             invoices = invoice_model.objects.filter(project=project)
-            times = get_times_for_obj(project, time_model)
+            times = time_model.objects.filter(
+                invoiced=False, estimate=None, project=obj, task__isnull=False)
             if order_by:
                 times = times.order_by(*order_by['time'])
                 invoices = invoices.order_by(*order_by['invoice'])
