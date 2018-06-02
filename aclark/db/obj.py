89b8451d0ba8dcb84f96579598288f82b62befc3
Alex Clark <aclark@aclark.net>
2018-06-02 11:48:25 -0400

    Update

--- aclark/db/obj.py
+++ aclark/db/obj.py
@@ -55,7 +55,7 @@ def get_times_for_obj(obj, time_model):
         times = time_model.objects.filter(estimate=None, invoice=obj)
     elif model_name == 'project':
         times = time_model.objects.filter(
-            invoiced=False, estimate=None, project=obj, task__isnull=False)
+            estimate=None, project=obj, task__isnull=False)
     return times
 
 
