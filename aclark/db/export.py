869c5880b8da866ccd586fb4285b7c38abc29556
Alex Clark <aclark@aclark.net>
2018-06-10 16:02:07 -0400

    Update

--- aclark/db/export.py
+++ aclark/db/export.py
@@ -33,19 +33,9 @@ def render_doc(context, **kwargs):
             document.add_heading(element.text, level=2)
         elif element.tag == 'p':
             document.add_paragraph(element.text)
-    response = HttpResponse(content_type='application/docx')
-    response['Content-Disposition'] = 'filename=%s' % filename
-    return response
-
-
-def render_doc(request, **kwargs):
-    document = Document()
-    document.add_heading('Document Title', 0)
-
     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
     response['Content-Disposition'] = 'attachment; filename=download.docx'
     document.save(response)
-
     return response
 
 
