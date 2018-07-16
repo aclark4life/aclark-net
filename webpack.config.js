2e5c6e892f4f98ed0bbf84a9f247216eed9db734
Alex Clark <aclark@aclark.net>
2018-07-16 08:14:08 -0400

    Revert "Update"
    This reverts commit 4ab63b2b33cb70ccbcda9cb718aa45f30f8a93d4.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,1 @@
-var path = require('path');
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
-
-module.exports = {
-  context: __dirname,
-  entry: './aclark/www/static/index',
-  output: {
-      path: path.resolve('./aclark/www/static/webpack_bundles/'),
-      filename: "[name]-[hash].js"
-  },
-
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'})
-  ]
-}
+module.exports = { entry: './app.js', output: { filename: 'bundle.js' } }
